f7096e99b583f12d2073ab06ef9c416d454fb35d
upender000 <upender000@gmail.com>
2015-04-23 21:31:52 -0500

    Added previous pictures and process image by reading from sd card

--- MoleDetection/src/com/moledetection/android/GetPreviousPictures.java
+++ MoleDetection/src/com/moledetection/android/GetPreviousPictures.java
@@ -1,116 +1,204 @@
 package com.moledetection.android;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
-import android.database.Cursor;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 public class GetPreviousPictures extends Activity {
-	/**
-     * Cursor used to access the results from querying for images on the SD card.
-     */
-    private Cursor cursor;
-    /**
-     * Column index for the Thumbnails Image IDs.
-     */
-    private int columnIndex;
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_get_previous_pictures);
-		
-		 // Set up an array of the Thumbnail Image ID column we want
-        String[] projection = {MediaStore.Images.Thumbnails._ID};
-        // Create the cursor pointing to the SDCard
-        cursor = managedQuery( MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                projection, // Which columns to return
-                null,       // Return all rows
-                null,
-                MediaStore.Images.Thumbnails.IMAGE_ID);
-        // Get the column index of the Thumbnails Image ID
-        columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+	public final static String FILEPATH = "MESSAGE";
+    private static final String mDirName = "MoleDetection";
+	List<String> tFileList;
+    public Bitmap bm;
+    ByteArrayOutputStream baos;
+    ImageView jpgView;
+    File f;
+    File[] files;
+    ImageView imageView;
+	protected String filePath;
+	private Button b2,proceed_button;
+	private GridView g;
 
-        GridView sdcardImages = (GridView) findViewById(R.id.get_pictures);
-        sdcardImages.setAdapter(new ImageAdapter(this));
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_previous_pictures);
 
-        // Set up a click listener
-        sdcardImages.setOnItemClickListener(new OnItemClickListener() {
-        	@SuppressWarnings("rawtypes")
-			@Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
-                // Get the data location of the image
-                String[] projection = {MediaStore.Images.Media.DATA};
-                cursor = getContentResolver().query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        projection, // Which columns to return
-                        null,       // Return all rows
-                        null,
-                        null);
-                columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToPosition(position);
-                // Get image filename
-                String imagePath = cursor.getString(columnIndex);
+        g = (GridView) findViewById(R.id.get_pictures);
+        g.setAdapter(new ImageAdapter(this, ReadSDCard()));
+        
+        //registerForContextMenu(g);
+        
+        
+
+        g.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View v,
+                    int position, long id) {
+                
+    
+                
+                b2 = (Button) findViewById(R.id.button2);
+                proceed_button = (Button) findViewById(R.id.proceed_button);
+                imageView = (ImageView) findViewById(R.id.full_image_view);
+                
+                g.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+                b2.setVisibility(View.VISIBLE);
+                proceed_button.setVisibility(View.VISIBLE);
+               // Toast.makeText(GetPreviousPictures.this, "" + position,
+                //        Toast.LENGTH_SHORT).show();
+                
+                System.out.println("Inside Onclick...............+position");
+
+                //setContentView(R.layout.full_image);
+                
+                addListenerOnButton2();
+                addListenerOnButtonProceed();
+
+    System.out.println("inside the full image?????????????????????????????????");
+
+
+                 //Intent i = new Intent(GetPreviousPictures.this, imageViewFlipper.class);
+                // passing array index
+                // i.putExtra("id", position);
+                //  startActivity(i);
+                System.out
+                        .println("Inside the intent////////////////////////////////////");
+                filePath = files[position].getPath();
+                bm = BitmapFactory.decodeFile(filePath);
+                imageView.setImageBitmap(bm);
+                
+                
+                System.out
+                        .println("Inside ihe image view///////////////////????????????????????????");
             }
-
-			
         });
+        //registerForContextMenu(g);
+        
+    }
+    
+    public void addListenerOnButton2() {
+		 
+		Button button = (Button) findViewById(R.id.button2);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				imageView.setVisibility(View.GONE);
+                b2.setVisibility(View.GONE);
+                proceed_button.setVisibility(View.GONE);
+                g.setVisibility(View.VISIBLE);
+			}
+ 
+		});
+ 
 	}
-	
-    /**
-     * Adapter for our image files.
-     */
-    private class ImageAdapter extends BaseAdapter {
+    
+    public void addListenerOnButtonProceed() {
+		 
+		Button button = (Button) findViewById(R.id.proceed_button);
+		button.setOnClickListener(new OnClickListener() {
 
-        private Context context;
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(GetPreviousPictures.this, ProcessImage.class);
+				i.putExtra(FILEPATH, filePath);
+				startActivity(i);
+			}
+ 
+		});
+ 
+	}
 
-        public ImageAdapter(Context localContext) {
-            context = localContext;
+    private List<String> ReadSDCard() {
+        tFileList = new ArrayList<String>();
+
+        // It have to be matched with the directory in SDCard
+        //f = new File("/data/digitechimages");// Here you take your specific folder//
+        f = new File(Environment.getExternalStorageDirectory(), "/" + mDirName);
+
+        files = f.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return ((name.endsWith(".jpg")) || (name.endsWith(".png")));
+            }
+        });
+
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            /*
+             * It's assumed that all file in the path are in supported type
+             */
+            tFileList.add(file.getPath());
+        }
+        return tFileList;
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        int mGalleryItemBackground;
+        private Context mContext;
+        private List<String> FileList;
+
+        public ImageAdapter(Context c, List<String> fList) {
+            mContext = c;
+            FileList = fList;
         }
 
         public int getCount() {
-            return cursor.getCount();
+            return FileList.size();
         }
+
         public Object getItem(int position) {
             return position;
         }
+
         public long getItemId(int position) {
             return position;
         }
-        @Override
+
         public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView picturesView;
+            ImageView imageView;
             if (convertView == null) {
-                picturesView = new ImageView(context);
-                // Move cursor to current position
-                cursor.moveToPosition(position);
-                // Get the current value for the requested column
-                int imageID = cursor.getInt(columnIndex);
-                // Set the content of the image based on the provided URI
-                picturesView.setImageURI(Uri.withAppendedPath(
-                        MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID));
-                picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                picturesView.setPadding(8, 8, 8, 8);
-                picturesView.setLayoutParams(new GridView.LayoutParams(100, 100));
+                imageView = new ImageView(mContext);
+                Bitmap bm = BitmapFactory.decodeFile(FileList.get(position)
+                        .toString());
+                imageView.setImageBitmap(bm);
+                // imageView.setImageResource(FileList[position]);
+                imageView.setLayoutParams(new GridView.LayoutParams(175, 175));
+                imageView.setAdjustViewBounds(true);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 5, 8, 5);
+                //imageView.setBackgroundResource(mGalleryItemBackground);
+
+            } else {
+                imageView = (ImageView) convertView;
             }
-            else {
-                picturesView = (ImageView)convertView;
-            }
-            return picturesView;
+
+            // imageView.setImageResource(fi[position]);
+
+            return imageView;
         }
-		
+
     }
+
 }
