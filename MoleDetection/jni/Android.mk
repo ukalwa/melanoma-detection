63769f7110c743a25388034bb9c291aa9de862fa
upender000 <upender000@gmail.com>
2015-05-18 15:03:01 -0500

    Local paths to env variables related path

--- MoleDetection/jni/Android.mk
+++ MoleDetection/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include C:/Users/Upender/Downloads/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+include $(OPENCV_SDK)/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := active_contour
 LOCAL_SRC_FILES := jni_part.cpp ac_withoutedges_yuv.cpp activecontour.cpp linked_list.cpp
