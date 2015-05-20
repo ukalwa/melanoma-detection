b66cc96734ab83da6815f2e9e12e69b27bcb0a4f
julianam <julianam@co1050-03.ece.iastate.edu>
2015-05-20 15:42:00 -0500

    Include the ColorDetection Class.

--- MoleDetection/obj/local/armeabi-v7a/objs/active_contour/ac_withoutedges_yuv.o.d
+++ MoleDetection/obj/local/armeabi-v7a/objs/active_contour/ac_withoutedges_yuv.o.d
@@ -1,14 +1,14 @@
-C:/Users/Upender/git/moledetection/MoleDetection//obj/local/armeabi-v7a/objs/active_contour/ac_withoutedges_yuv.o: \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.cpp \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.hpp \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/activecontour.hpp \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/linked_list.hpp \
- C:/Android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream
+U:/git/moledetection/MoleDetection//obj/local/armeabi-v7a/objs/active_contour/ac_withoutedges_yuv.o: \
+ U:/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.cpp \
+ U:/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.hpp \
+ U:/git/moledetection/MoleDetection//jni/activecontour.hpp \
+ U:/git/moledetection/MoleDetection//jni/linked_list.hpp \
+ C:/Users/Public/Downloads/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream
 
-C:/Users/Upender/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.hpp:
+U:/git/moledetection/MoleDetection//jni/ac_withoutedges_yuv.hpp:
 
-C:/Users/Upender/git/moledetection/MoleDetection//jni/activecontour.hpp:
+U:/git/moledetection/MoleDetection//jni/activecontour.hpp:
 
-C:/Users/Upender/git/moledetection/MoleDetection//jni/linked_list.hpp:
+U:/git/moledetection/MoleDetection//jni/linked_list.hpp:
 
-C:/Android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+C:/Users/Public/Downloads/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
--- MoleDetection/obj/local/armeabi-v7a/objs/active_contour/linked_list.o.d
+++ MoleDetection/obj/local/armeabi-v7a/objs/active_contour/linked_list.o.d
@@ -1,8 +1,8 @@
-C:/Users/Upender/git/moledetection/MoleDetection//obj/local/armeabi-v7a/objs/active_contour/linked_list.o: \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/linked_list.cpp \
- C:/Android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
- C:/Users/Upender/git/moledetection/MoleDetection//jni/linked_list.hpp
+U:/git/moledetection/MoleDetection//obj/local/armeabi-v7a/objs/active_contour/linked_list.o: \
+ U:/git/moledetection/MoleDetection//jni/linked_list.cpp \
+ C:/Users/Public/Downloads/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ U:/git/moledetection/MoleDetection//jni/linked_list.hpp
 
-C:/Android/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+C:/Users/Public/Downloads/android-ndk-r10d/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
 
-C:/Users/Upender/git/moledetection/MoleDetection//jni/linked_list.hpp:
+U:/git/moledetection/MoleDetection//jni/linked_list.hpp:
